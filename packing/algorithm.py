34b4ff6dc6c29eff45f131f615f40e8e8146f706
Mira <szewczyk.mira@gmail.com>
2020-04-21 21:27:00 +0200

    Define new implementation with itertools

--- packing/algorithm.py
+++ packing/algorithm.py
@@ -1,4 +1,5 @@
 import copy
+from itertools import combinations
 from typing import List
 
 from .basket import Basket, Item
@@ -20,3 +21,21 @@ def create_all_combinations(items: List[Item]) -> List[Basket]:
         combinations.extend(create_combinations_with_item(item, combinations))
     combinations.remove(empty_basket)
     return combinations
+
+
+# Algorithm v 2.0 to check what options will be faster
+def convert_to_baskets(combinations: List[List[Item]]) -> List[Basket]:
+    baskets = []
+    for combination in combinations:
+        baskets.append(
+            Basket(items=combination, cost=sum([item.price for item in combination]))
+        )
+    return baskets
+
+
+def create_combinations_with_itertools(items: List[Item]) -> List[Basket]:
+    all_combinations = []
+    for i in range(1, len(items) + 1):
+        all_combinations.extend(list(combinations(items, i)))
+
+    return convert_to_baskets(all_combinations)
--- packing/test_algorithm.py
+++ packing/test_algorithm.py
@@ -1,18 +1,34 @@
 from math import comb
 
-from .algorithm import create_all_combinations
+import pytest
+
+from .algorithm import (
+    create_all_combinations,
+    create_combinations_with_itertools,
+)
 from .basket import Item
 
 
-def test_generate_all_combinations_returns_proper_amount():
-    items = [
+@pytest.fixture
+def items():
+    return [
         Item(1),
         Item(2),
         Item(3),
         Item(4),
         Item(5),
     ]
+
+
+def test_generate_all_combinations_returns_proper_amount(items):
     items_len = len(items)
     combinations = create_all_combinations(items)
+    expected_output = sum([comb(items_len, i) for i in range(1, items_len + 1)])
+    assert len(combinations) == expected_output
+
+
+def test_generate_all_combinations_with_itertools(items):
+    items_len = len(items)
+    combinations = create_combinations_with_itertools(items)
     expected_output = sum([comb(items_len, i) for i in range(1, items_len + 1)])
     assert len(combinations) == expected_output
