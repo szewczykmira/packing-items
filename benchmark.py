3bafaa85b4ea8f4918590406394fecce738fd61e
Mira <szewczyk.mira@gmail.com>
2020-04-22 16:17:58 +0200

    Add more benchmarks

--- benchmark.py
+++ benchmark.py
@@ -1,17 +1,55 @@
 import timeit
 
-from packing import combinations_v1, combinations_v2
+from packing import algorithm_v12, combinations_v1, combinations_v2
 from packing.basket import Item
+from termcolor import colored
 
-if __name__ == "__main__":
+items = [
+    Item(1),
+    Item(2),
+    Item(3),
+    Item(4),
+    Item(5),
+]
 
-    items = [
-        Item(1),
-        Item(2),
-        Item(3),
-        Item(4),
-        Item(5),
-    ]
+print(colored("Most naive version of solution", "yellow"))
+comb_v1 = lambda: combinations_v1.create_all(items)
+affordable_combinations_v1 = lambda: algorithm_v12.get_affordable_baskets(
+    10, combinations_v1.create_all(items)
+)
+print(
+    colored(
+        "\t generating all combinations: {}".format(
+            timeit.timeit(comb_v1, number=3000)
+        ),
+        "yellow",
+    )
+)
+print(
+    colored(
+        "\t generating all combinations and filtering relevant one: {}\n".format(
+            timeit.timeit(affordable_combinations_v1, number=3000,)
+        ),
+        "yellow",
+    )
+)
 
-    print(timeit.timeit(lambda: combinations_v1.create_all(items), number=3000))
-    print(timeit.timeit(lambda: combinations_v2.create_all(items), number=3000))
+
+print(colored("Implementation using itertools", "white"))
+comb_v2 = lambda: combinations_v2.create_all(items)
+affordable_combinations_v2 = lambda: algorithm_v12.get_affordable_baskets(
+    10, combinations_v2.create_all(items)
+)
+
+print(
+    colored(
+        "\t Generating all combinations: {}".format(timeit.timeit(comb_v2, number=3000))
+    )
+)
+print(
+    colored(
+        "\t Generating all combinations and filtering relevant one: {}".format(
+            timeit.timeit(affordable_combinations_v2, number=3000)
+        )
+    )
+)
